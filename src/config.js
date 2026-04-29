8e371d580ecd1772c4f9ce0c0749faa0246bf3e7
Max Bos <143814729+Max-Bos@users.noreply.github.com>
2026-04-29 10:24:05 +0200

    Update config.js
    Removed credentials from config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,1 @@
-// ── Vul hier je Supabase credentials in ──────────────────
-// Te vinden in: Supabase dashboard → Project Settings → API
-export const SUPABASE_URL     = 'https://avgmorbubrpmthpyxtxb.supabase.co'
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2Z21vcmJ1YnJwbXRocHl4dHhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0NDk1OTUsImV4cCI6MjA5MzAyNTU5NX0.Gd_IOdiRNx0mdqUQdccR_7Xx9LVCTezfhGfw_DOP194'
+
